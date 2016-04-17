08fa11683dc2b385952e933faa4dd495c06219ff
edhowland <ed.howland@gmail.com>
2016-04-17 20:42:03 +0000

    \'reduced location finder in FaultDetail\'

--- lib/minitest/reporters/json_reporter/fault_detail.rb
+++ lib/minitest/reporters/json_reporter/fault_detail.rb
@@ -1,21 +1,28 @@
 # fault_detail.rb - class MiniTest::Reporters::FaultDetail
 
-# TODO module documentation
+# MiniTest Base namespace for Minitest related objects
 module MiniTest
-  # TODO module documentation
+  # Reporters - Extensible Reporting framework for Minitest
   module Reporters
-    # TODO: class documentation
+    # FaultDetail - Subclass of TestDetail and superclass for SkipDetail, FailDetail and ErrorDetail
     class FaultDetail < TestDetail
-      def initialize test, exception
+      def initialize test
+      # Initialize for  FaultDetail - sets up exception stuff: message, location and backtrace
         super test
-        @message = exception.message
-        @backtrace = exception.backtrace
+        @exception = test.failure
+        @message = @exception.message
+        @backtrace = @exception.backtrace
+      end
+
+      # returns string of found location of this test from backtrace
+      def location
+        @backtrace.reverse.reduce('') {|a, e| break a if e =~ /assert/; e.sub(/:in .*$/, '') }
       end
 
         def to_h
         h = super
         h[:message] = @message
-        h[:location] = ''
+        h[:location] = location
         h
       end
 
--- test/fault_detail_spec.rb
+++ test/fault_detail_spec.rb
@@ -2,16 +2,34 @@
 
 require_relative 'spec_helper'
 
-# TODO: class documentation
+def assert expr
+  fail('bad juju') unless expr
+end
+
+#binding.pry
+# mk_exc - create an exception for use in  these tests
+def mk_exc message
+  assert false
+  rescue => err
+    err
+end
+
+
+# FaultyTest (apologies  to Jhon Cleese) A Minitest::Test fake
 class FaultyTest
+  def initialize 
+    @failure = mk_exc('bad juju')
+  end
+
+  attr_reader :failure
+
   def name; 'juju'; end
 end
 
 describe MiniTest::Reporters::FaultDetail do
   describe 'to_h' do
-    let(:exc) { RuntimeError.new 'bad juju' }
     let(:tst) { FaultyTest.new }
-    let(:det) { MiniTest::Reporters::FaultDetail.new(tst, exc) }
+    let(:det) { MiniTest::Reporters::FaultDetail.new(tst)  }
     it 'should have hash with superclass' do
       det.to_h[:name].must_equal 'juju'
       det.to_h[:class].must_equal 'FaultyTest'
@@ -20,6 +38,11 @@ describe MiniTest::Reporters::FaultDetail do
     it 'should have exception stuff in hash' do
       det.to_h[:message].must_equal 'bad juju'
     end
+
+    it 'should have location: fault_detail_spec.rb' do
+      det.to_h[:location].must_equal 'fault_detail_spec.rb:12'
+end
+
 
 
 end
