349d68b93845cd40382394980f1b38812ffc23dd
edhowland <ed.howland@gmail.com>
2016-04-20 19:13:02 +0000

    \'working test task fro Rakefile\'

--- Rakefile
+++ Rakefile
@@ -1,1 +1,9 @@
+# Rakefile - tasks for minitest-reporters-json_reporter gem
 require 'bundler/gem_tasks'
+
+
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.pattern = './test/*_spec.rb'
+end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,4 +1,6 @@
 # spec_helper.rb - helper harness for minispec for minitest-reporters-json_reporter
+# Possibly require SimpleCov libs if user has ~/.load_simplecov
+require_relative 'load_simplecov' if File.exist?(File.expand_path('~/.load_simplecov'))
 
 require_relative 'load_path'
 
