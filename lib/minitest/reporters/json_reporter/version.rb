c67fe04ed0f9f9554937ffa5abb80c9f5b543835
edhowland <ed.howland@gmail.com>
2016-05-17 19:03:17 +0000

    \'added badge for code climate and bumped version to 0.9.9\'

--- lib/minitest/reporters/json_reporter/version.rb
+++ lib/minitest/reporters/json_reporter/version.rb
@@ -8,7 +8,7 @@ module Minitest
     class JsonReporter < BaseReporter
       ##
       # Version of the Minitest::Reporters::JsonReporter gem.
-      VERSION = '0.9.8'.freeze
+      VERSION = '0.9.9'.freeze
     end
   end
 end
